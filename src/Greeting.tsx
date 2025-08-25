4ab2695dba70cf88c3b02fbb4e1ef285859e8cf4
lalinyuuu <lullalin.yu@gmail.com>
2025-08-25 13:27:08 +0700

    ex1

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -1,23 +1,25 @@
-// Exercise1.tsx
 import { useState } from "react";
 
-// TODO1:  ประกาศ Type ด้วย type หรือ inerface
+// TODO1: ประกาศ Type ให้ props
+export interface GreetingProps {
+  name: string;
+  age: number;
+}
 
 // TODO2: กำหนด Type ให้ props
-const Greeting = (props) => {
+const Greeting: React.FC<GreetingProps> = ({ name, age }) => {
   // TODO3: กำหนด Type ให้ useState
-  const [count, setCount] = useState();
+  const [count, setCount] = useState<number>(0);
 
   return (
     <div>
       <h1>
-        Hello {props.name}, you are {props.age} years old 👋
+        Hello {name}, you are {age} years old 👋
       </h1>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <button onClick={() => setCount((c) => c + 1)}>Click Me</button>
     </div>
   );
 };
 
-// TODO4 : Render ออกมาใน App.tsx
 export default Greeting;
